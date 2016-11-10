4ae0c7e04f56cea918abf181eef0255fcadfbace
lili <liq1862@gmail.com>
2016-11-10 13:18:09 +0800

    FrameLayout

--- app/src/main/java/com/example/user/a20161110_test01/MainActivity.java
+++ app/src/main/java/com/example/user/a20161110_test01/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -13,27 +14,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout4);
 
-        LinearLayout ll = new LinearLayout(MainActivity.this);
-        ll.setOrientation(LinearLayout.HORIZONTAL);
 
-        LinearLayout.LayoutParams  params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT,1);
-
-        //LinearLayout ll = (LinearLayout) findViewById(R.id.layout2_linear);
-        Button bt = new Button(MainActivity.this);
-        bt.setText("Button");
-        ll.addView(bt);
-        Button bt2 = new Button(MainActivity.this);
-        bt2.setText("Button!!");
-        ll.addView(bt2);
-
-        setContentView(ll);
+//        LinearLayout ll = new LinearLayout(MainActivity.this);
+//        ll.setOrientation(LinearLayout.HORIZONTAL);
+//
+//        LinearLayout.LayoutParams  params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+//                ViewGroup.LayoutParams.WRAP_CONTENT,1);
+//
+//        //LinearLayout ll = (LinearLayout) findViewById(R.id.layout2_linear);
+//        Button bt = new Button(MainActivity.this);
+//        bt.setText("Button");
+//        ll.addView(bt);
+//        Button bt2 = new Button(MainActivity.this);
+//        bt2.setText("Button!!");
+//        ll.addView(bt2);
+//
+//        setContentView(ll);
 
 //        Button btn = new Button(MainActivity.this);
 //        btn.setText("Click me!");
 //        setContentView(btn);
-
-
+    }
+    public void click1(View v)
+    {
+        v.setVisibility(View.INVISIBLE);
     }
 }
